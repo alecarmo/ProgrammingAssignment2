46053d80f437a717ffd5199eb2e6665ca51c5604
Alexandre do Carmo <alecarmo@msn.com>
2015-04-26 09:45:52 -0300

    Update cachematrix.R
    Attribution: Cache the inverse of a matrix.
    
    For solution I used the MakeChacheMatrix and CacheSolve functions. To test the solution made according to a test that simulates an inverted input matrix and its inverse returning further testing using the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(a = matrix()) {
+    i <- NULL
+    temp <- a[1]
+    set <- function(a) {
+        i <<- NULL
+        temp <- a[1]
+    }
+    get <- function() a
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
+cacheSolve <- function(of, ...) {
+    i2 <- of$getinverse()
+    if(!is.null(i2)) {
+        message("getting cached data")
+        return(i2)}
+    else message("caching data")
+    data <- of$get()
+    i2 <- solve(data, ...)
+    of$setinverse(i2)
+    i2
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Test caching
+test <- function() {
+  X <- matrix(c(3,-2,-4,3),2,2)
+  Mat <- makeCacheMatrix(X)
+  for (n in 1:3) {
+    Y <- cacheSolve(Mat)
+    print(Y)
+  }
 }
